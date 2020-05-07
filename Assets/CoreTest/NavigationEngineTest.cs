7a202b1b3b5ac81a975e3c533bf58cf794e93db5
LDZ <loic.dalzotto@hotmail.fr>
2020-05-07 23:45:46 +0200

    Adding a simple test that check health recovery trigger

--- Assets/CoreTest/NavigationEngineTest.cs
+++ Assets/CoreTest/NavigationEngineTest.cs
@@ -1,5 +1,16 @@
-﻿using _GameLoop;
+﻿using _ActionPoint;
+using _Attack;
+using _Entity;
+using _Entity._Events;
+using _EntityCharacteristics;
+using _EventQueue;
+using _GameLoop;
+using _Health;
+using _HealthRecovery;
+using _Locomotion;
+using _NavigationEngine;
 using _NavigationGraph;
+using _TurnTimeline;
 using NUnit.Framework;
 using System.Collections.Generic;
 using Unity.Mathematics;
@@ -7,17 +18,36 @@ using UnityEngine;
 
 public class NavigationTest
 {
+    public static EventQueue TestEventQueue;
+
     [SetUp]
     public void Before()
     {
+        TestEventQueue = EventQueue.alloc();
         ExternalHooks.LogDebug = (string s) => { };
-
         _TurnTimeline.TurnTimeline.alloc();
-
+        NavigationEngine.alloc();
         NavigationGraph l_navigationGraph = NavigationGraph.alloc();
         NavigationGraphContainer.UniqueNavigationGraph = l_navigationGraph;
         CreateGridNavigation(l_navigationGraph, 4, 4);
         NavigationGraph.takeSnapshot(l_navigationGraph);
+    }
+
+    [TearDown]
+    public void After()
+    {
+        foreach (Entity l_entity in EntityContainer.Entities)
+        {
+            Entity.markForDestruction(l_entity);
+            EventQueue.enqueueEvent(TestEventQueue, EntityDestroyEvent.alloc(l_entity));
+        }
+
+        EventQueue.iterate(TestEventQueue);
+        EventQueue.clearAll(TestEventQueue);
+        _TurnTimeline.TurnTimeline.free(_TurnTimeline.TurnTimelineContainer.UniqueTurnTimeline);
+        NavigationEngine.free(NavigationEngineContainer.UniqueNavigationEngine);
+        NavigationGraph.free(NavigationGraphContainer.UniqueNavigationGraph);
+        TestEventQueue = null;
     }
 
     private void CreateGridNavigation(NavigationGraph p_navigationGraph, int XNumber, int ZNumber)
@@ -102,77 +132,57 @@ public class NavigationTest
         }
     }
 
-#if comment
-
-    [TearDown]
-    public void After()
-    {
-        foreach (Entity l_entity in EntityContainer.Entities)
-        {
-            Entity.markForDestruction(l_entity);
-        }
-
-        EventQueue.iterate(EventQueue.UniqueInstance);
-        EventQueue.clearAll(EventQueue.UniqueInstance);
-
-        _TurnTimeline.TurnTimeline.free(_TurnTimeline.TurnTimelineContainer.UniqueTurnTimeline);
-        NavigationGraph.free(NavigationGraphContainer.UniqueNavigationGraph);
-    }
-
     [Test]
-    public void MyTest()
+    public void NavigationTriggerComponentTest()
     {
+        // Health trigger
+        Entity l_healthTrigger = Entity.alloc();
+
+        HealthRecoveryData l_healthRecoveryData = new HealthRecoveryData() { RecoveredHealth = 10.0f };
+        EntityComponent.add_component(l_healthTrigger, HealthRecovery.alloc(l_healthRecoveryData)); 
+        EntityComponent.add_component(l_healthTrigger, Locomotion.alloc(Locomotion.EMPTY_MOVE_TO_NAVIGATION_NODE, Locomotion.EMPTY_WARP));
+        
+        EventQueue.enqueueEvent(
+          TestEventQueue,
+          NavigationNodeWarpEntityEvent.alloc(
+              l_healthTrigger,
+              NavigationGraphAlgorithm.getNearestNode(
+                  NavigationGraphContainer.UniqueNavigationGraph,
+                  new float3(1.0f, 0.0f, 1.0f)
+                  )
+          )
+      );
+
+        EventQueue.iterate(TestEventQueue);
+
+        //
+
         Entity l_entity1 = Entity.alloc();
         EntityCharacteristicsData l_entityCharacteristicsData = new EntityCharacteristicsData() { Speed = 20.0f };
         TurnTimelineElligibilityData l_turnTimelineElligibilityData = TurnTimelineElligibilityData.build(true);
         ActionPointData l_actionPointData = new ActionPointData() { InitialActionPoints = 3.0f };
         AttackData l_attackData = new AttackData() { APCost = 1.0f, Damage = 1.0f };
+        HealthData l_healthData = new HealthData() { MaxHealth = 100.0f, CurrentHealth = 100.0f };
 
         EntityComponent.add_component(l_entity1, EntityCharacteristics.alloc(ref l_entityCharacteristicsData));
         EntityComponent.add_component(l_entity1, TurnTimelineElligibility.alloc(ref l_turnTimelineElligibilityData));
         EntityComponent.add_component(l_entity1, ActionPoint.alloc(ref l_actionPointData));
         EntityComponent.add_component(l_entity1, Locomotion.alloc(Locomotion.EMPTY_MOVE_TO_NAVIGATION_NODE, Locomotion.EMPTY_WARP));
         EntityComponent.add_component(l_entity1, Attack.alloc(ref l_attackData));
+        EntityComponent.add_component(l_entity1, Health.alloc(ref l_healthData));
 
-        EventQueue.insertEventAt(
-            EventQueue.UniqueInstance,
-            0,
+        EventQueue.enqueueEvent(
+            TestEventQueue,
             NavigationNodeWarpEntityEvent.alloc(
                 l_entity1,
-                NavigationGraphAlgorithm.getNearestNode(
-                    NavigationGraphContainer.UniqueNavigationGraph,
-                    new float3(1.0f, 0.0f, 1.0f)
-                    )
-                )
-            );
+                l_healthTrigger.CurrentNavigationNode
+            )
+        );
 
+        EventQueue.iterate(TestEventQueue);
 
-
-        Entity l_entity2 = Entity.alloc();
-        EntityComponent.add_component(l_entity2, EntityCharacteristics.alloc(ref l_entityCharacteristicsData));
-        EntityComponent.add_component(l_entity2, TurnTimelineElligibility.alloc(ref l_turnTimelineElligibilityData));
-        EntityComponent.add_component(l_entity2, ActionPoint.alloc(ref l_actionPointData));
-        EntityComponent.add_component(l_entity2, Locomotion.alloc(Locomotion.EMPTY_MOVE_TO_NAVIGATION_NODE, Locomotion.EMPTY_WARP));
-        EntityComponent.add_component(l_entity2, Attack.alloc(ref l_attackData));
-
-        EventQueue.insertEventAt(
-            EventQueue.UniqueInstance,
-            0,
-            NavigationNodeWarpEntityEvent.alloc(
-                l_entity2,
-                NavigationGraphAlgorithm.getNearestNode(
-                    NavigationGraphContainer.UniqueNavigationGraph,
-                    new float3(1.0f, 0.0f, 2.0f)
-                    )
-                )
-            );
-
-
-        EventQueue.iterate(EventQueue.UniqueInstance);
-
-
-        EventQueue.insertEventAt(EventQueue.UniqueInstance, 0, StartEntityTurnEvent.alloc(l_entity1));
-        EventQueue.iterate(EventQueue.UniqueInstance);
+        Assert.IsTrue(EntityComponent.get_component<Health>(l_entity1).HealthData.CurrentHealth == 110.0f);
+        Assert.IsTrue(!EntityContainer.Entities.Contains(l_healthTrigger));
     }
-#endif
+
 }
